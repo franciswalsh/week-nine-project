af5001e0ad38ff15cdd90c51e3c6edf67b7ac41a
Frank <francisw396@gmail.com>
2017-07-24 12:17:34 -0400

    loading data to html successfully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -61,6 +58,81 @@ public class Main {
                 counter++;
             }
         }
-        System.out.println(allVehicles[4].getVin());
+        int numberOfVehicles = allVehicles.length;
+        double totalOdometer = 0;
+        double totalConsumption = 0;
+        double totalOdometerAtLastOilChange = 0;
+        double totalEngineSize = 0;
+
+        for (VehicleInfo vehicle : allVehicles){
+            totalOdometer += vehicle.getOdometer();
+            totalConsumption += vehicle.getConsumption();
+            totalOdometerAtLastOilChange += vehicle.getOdometerAtLastOilChange();
+            totalEngineSize += vehicle.getEngineSize();
+        }
+
+        double averageOdometer = Math.round((totalOdometer / numberOfVehicles)*10.0)/10.0;
+        double averageConsumption = Math.round((totalConsumption / numberOfVehicles)*10.0)/10.0;
+        double averageOdometerAtLastOilChange = Math.round((totalOdometerAtLastOilChange / numberOfVehicles)*10.0)/10.0;
+        double averageEngineSize = Math.round((totalEngineSize / numberOfVehicles)*10.0)/10.0;
+
+
+        try {
+            File htmlFile = new File("dashboard.html");
+            FileWriter htmlFileWriter = new FileWriter(htmlFile);
+
+              htmlFileWriter.write("<html>\n" +
+                      "  <title>Vehicle Telematics Dashboard</title>\n" +
+                      "  <body>");
+              // need number of vehicles
+              htmlFileWriter.write("<h1 align=\"center\">Averages for " +
+                      allVehicles.length +
+                      " vehicles</h1>");
+              htmlFileWriter.write("<table align=\"center\">\n" +
+                      "        <tr>\n" +
+                      "            <th>Odometer (miles) |</th><th>Consumption (gallons) |</th><th>Last Oil Change |</th><th>Engine Size (liters)</th>\n" +
+                      "        </tr>");
+              //need averages
+              htmlFileWriter.write("<tr>\n" +
+                      "            <td align=\"center\">" +
+                      averageOdometer +
+                      "</td><td align=\"center\">" +
+                      averageConsumption +
+                      "</td><td align=\"center\">" +
+                      averageOdometerAtLastOilChange +
+                      "</td align=\"center\"><td align=\"center\">" +
+                      averageEngineSize +
+                      "</td>\n" +
+                      "        </tr>");
+              htmlFileWriter.write("</table>");
+              htmlFileWriter.write("<h1 align=\"center\">History</h1>\n" +
+                      "    <table align=\"center\" border=\"1\">\n" +
+                      "        <tr>\n" +
+                      "            <th>VIN</th><th>Odometer (miles)</th><th>Consumption (gallons)</th><th>Last Oil Change</th><th>Engine Size (liters)</th>\n" +
+                      "        </tr>");
+              //need to loop through all vehicles and print info
+            for(VehicleInfo vehicle : allVehicles){
+                htmlFileWriter.write("<tr>\n" +
+                        "            <td align=\"center\">" +
+                        vehicle.getVin() +
+                        "</td><td align=\"center\">" +
+                        Math.round(vehicle.getOdometer()*10.0)/10.0 +
+                        "</td><td align=\"center\">" +
+                        Math.round(vehicle.getConsumption()*10.0)/10.0 +
+                        "</td><td align=\"center\">" +
+                        Math.round(vehicle.getOdometerAtLastOilChange()*10.0)/10.0 +
+                        "</td align=\"center\"><td align=\"center\">" +
+                        Math.round(vehicle.getEngineSize()*10.0)/10.0 +
+                        "</td>\n" +
+                        "        </tr>");
+            }
+            htmlFileWriter.write(" </table>\n" +
+                    "  </body>\n" +
+                    "</html>");
+            htmlFileWriter.close();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
